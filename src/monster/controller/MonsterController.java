a22fb3f689d16df03aa05819fa906d009accba7d
LokiTheZorua <31518758+LokiTheZorua@users.noreply.github.com>
2017-09-15 13:37:17 -0600

    added a scanner so the user can respond

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -1,6 +1,7 @@
 package monster.controller;
 
 import monster.model.MarshmallowMonster;
+import java.util.Scanner;
 
 public class MonsterController
 {
@@ -8,8 +9,21 @@ public class MonsterController
 	{
 		MarshmallowMonster basic = new MarshmallowMonster();
 		System.out.println(basic);
-		MarshmallowMonster John = new MarshmallowMonster("John", 7, 7, 6.7, true);
+		MarshmallowMonster John = new MarshmallowMonster("John", 2, 5, 3, true);
 		System.out.println (John);
+		System.out.println ("I am feeling hungry, I am going to eat one of his arms.");
+		John.setArmCount(John.getArmCount()- 1);
+		System.out.println(John);
+	}
+	
+	private void interactWithMonster(MarshmallowMonster currentMonster)
+	{
+		Scanner myScanner = new Scanner(System.in);
+		System.out.println(currentMonster.getname() + " wants to know how many eyes you want to eat, please type in how many.");
+		int consumed = myScanner.nextInt();
+		currentMonster.setEyeCount(currentMonster.getEyeCount()- consumed);
+		System.out.println(currentMonster);
 		
+		myScanner.close();
 	}
 }
--- src/monster/model/MarshmallowMonster.java
+++ src/monster/model/MarshmallowMonster.java
@@ -24,6 +24,46 @@ public class MarshmallowMonster
 		this.hasBloop = hasBloop;
 	}
 	
+	public String getname ()
+	{
+		return name;
+	}
+	public int getEyeCount()
+	{
+		return eyeCount;
+	}
+	public int getArmCount()
+	{
+		return armCount;
+	}
+	public double getTentaAmount()
+	{
+		return tentaAmount;
+	}
+	public boolean hasBloop()
+	{
+		return hasBloop;
+	}
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	public void setEyeCount(int eyeCount)
+	{
+		this.eyeCount = eyeCount;
+	}
+	public void setArmCount(int armCount)
+	{
+		this.armCount = armCount;
+	}
+	public void setTentaAmout(double tentaAmount)
+	{
+		this.tentaAmount = tentaAmount;
+	}
+	public void setHasBloop(boolean hasBloop)
+	{
+		this.hasBloop = hasBloop;
+	}
 	public String toString()
 	{
 		String description= "I am barely a moster, my name is " + name + ", I have " + eyeCount + " eyes and only " ;
