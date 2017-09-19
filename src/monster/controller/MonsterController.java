7117782e75b5bb184380c8b1220309a1e0a9eb4d
LokiTheZorua <31518758+LokiTheZorua@users.noreply.github.com>
2017-09-19 12:40:36 -0600

    added if, else if, and else statemnts

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -14,6 +14,7 @@ public class MonsterController
 		System.out.println ("I am feeling hungry, I am going to eat one of his arms.");
 		John.setArmCount(John.getArmCount()- 1);
 		System.out.println(John);
+		
 	}
 	
 	private void interactWithMonster(MarshmallowMonster currentMonster)
@@ -23,6 +24,30 @@ public class MonsterController
 		int consumed = myScanner.nextInt();
 		currentMonster.setEyeCount(currentMonster.getEyeCount()- consumed);
 		System.out.println(currentMonster);
+		
+		System.out.println("How many arms are you interested in eating? I have " + currentMonster.getArmCount());
+		//consumed = myScanner.nextInt();
+		int armEat = myScanner.nextInt();
+		
+		if(armEat == 0)
+		{
+			System.out.println("Not hungry? Oh... ok...");
+		}
+		
+		else if (armEat<0)
+		{
+			System.out.println("Reality is hard for you - it is impossible to eat a negative amount");
+		}
+		
+		else if (armEat > currentMonster.getArmCount() )
+		{
+			System.out.println("You cant eat more arms that I have!");
+		}
+		else
+		{
+			currentMonster.setArmCount(currentMonster.getArmCount() - armEat);
+			System.out.println("Ok, I now have " + currentMonster.getArmCount() + "arms" ); 
+		}
 		
 		myScanner.close();
 	}
