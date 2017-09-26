2356136d8db3439b488187fab1b739b7a5f1e36c
LokiTheZorua <31518758+LokiTheZorua@users.noreply.github.com>
2017-09-26 13:40:42 -0600

    added more validity loops

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -18,12 +18,12 @@ public class MonsterController
 //		while(count < 10)
 //		{
 //			count =+ 2;
-//			popup.displayText("Am I not the coolst?");
+//			popup.displayText("Am I not the coolest?");
 //		}
 		
 		for(int loop = 0; loop < 10; loop += 1)
 		{
-			popup.displayText("This is loop #" + (loop+ 1) + "of ten");
+			popup.displayText("This is loop #" + (loop+ 1) + " of ten");
 		}
 		
 		
@@ -46,89 +46,103 @@ public class MonsterController
 	
 	private void interactWithMonster(MarshmallowMonster currentMonster)
 	{
-		
+//		
 		Scanner myScanner = new Scanner(System.in);
-//		System.out.println(currentMonster.getname() + " wants to know how many eyes you want to eat, please type in how many.");
+		System.out.println(currentMonster.getname() + " wants to know how many eyes you want to eat, please type in how many.");
 		int consumed = 0;
 		String response = popup.getResponse(currentMonster.getname() + " wants to know how many eyes you want to eat, please type in how many.");
-		
+//		
 		while(!isValidInteger(response))
 		{
 			popup.displayText("Grrr, please type in the correct thing");
 			response = popup.getResponse("Type in an integer vaule!"); 
 		}
+		consumed = myScanner.nextInt();
 		
-		if(isValidInteger(response))
+		while(isValidInteger(response))
 		{
 			consumed = Integer.parseInt(response);
 		}
 		
-//		int consumed = myScanner.nextInt();
 		currentMonster.setEyeCount(currentMonster.getEyeCount()- consumed);
 //		System.out.println(currentMonster);
 		popup.displayText(currentMonster.toString());
 		int armEat = 0;
-//		System.out.println("How many arms are you interested in eating? I have " + currentMonster.getArmCount());
+		System.out.println("How many arms are you interested in eating? I have " + currentMonster.getArmCount());
 		String armResponse = popup.getResponse("How many arms are you interested in eating? I have " + currentMonster.getArmCount());
-		//consumed = myScanner.nextInt();
-
+		consumed = myScanner.nextInt();
+		
+		while(!isValidInteger(response))
+		{
+			popup.displayText("Please put in somehthing correct");
+			response = popup.getResponse("Please put something valid");
+		}
+		armEat = Integer.parseInt(armResponse);
+		currentMonster.setArmCount(currentMonster.getArmCount() - armEat);
+		System.out.println("Ok, I now have " + currentMonster.getArmCount() + " arms" ); 
+		
+		
 //		
 //		if(isVaildInteger(armResponse)
 //				{
 //			armEat = Integer.parseInt(armResponse);
 //	
 
-		// makes the if then stuff
-		if(armEat == 0)
-		{
-//			System.out.println("Not hungry? Oh... ok...");
-			popup.displayText("Not hungry? Oh... ok...");
-		}
+//		// makes the if then stuff
+//		if(armEat == 0)
+//		{
+////			System.out.println("Not hungry? Oh... ok...");
+//			popup.displayText("Not hungry? Oh... ok...");
+//		}
+//		
+//		else if (armEat<0)
+//		{
+////			System.out.println("Reality is hard for you - it is impossible to eat a negative amount");
+//			popup.displayText(" Reality is hard for oyu - it is impossible to eat a negative amount");
+//		}
+//		
+//		else if (armEat > currentMonster.getArmCount() )
+//		{
+////			System.out.println("You cant eat more arms that I have!");
+//			popup.displayText("You can't eat more arms than I have!");
+//		}
+//		else
+	
 		
-		else if (armEat<0)
-		{
-//			System.out.println("Reality is hard for you - it is impossible to eat a negative amount");
-			popup.displayText(" Reality is hard for oyu - it is impossible to eat a negative amount");
-		}
-		
-		else if (armEat > currentMonster.getArmCount() )
-		{
-//			System.out.println("You cant eat more arms that I have!");
-			popup.displayText("You can't eat more arms than I have!");
-		}
-		else
-		{
-			currentMonster.setArmCount(currentMonster.getArmCount() - armEat);
-//			System.out.println("Ok, I now have " + currentMonster.getArmCount() + " arms" ); 
-			popup.displayText("Ok, I now have " + currentMonster.getArmCount() + " arms");
-		}
-		
-//		System.out.println( "How many tenticles do you want to give me?");
+		popup.displayText("Ok, I now have " + currentMonster.getArmCount() + " arms");
+		System.out.println( "How many tenticles do you want to give me?");
 		popup.displayText("How many tenticles do you want to give me?");
 		//consumed = myScanner.nextInt();
 		double tentaGive = 0.0;
 		
-		if(tentaGive == 0)
+		while (!isValidInteger(response))
 		{
-//			System.out.println("Ok, that's fine");
-			popup.displayText("Ok, that's fine");
+			popup.displayText("Come on, just say somethin correct!");
+			popup.getResponse("How many arms do you want to give me?");
+			
 		}
-		
-		else if (tentaGive < 0)
-		{
-//			System.out.println("Sorry but I won't let you take any of my tenticles.");
-			popup.displayText("Sorry but I won't let you take any of my tenticles.");
-		}
-		else
-		{
-			currentMonster.setTentaAmout(currentMonster.getTentaAmount() + tentaGive);
-//			System.out.println("Thank you! I now have " + currentMonster.getTentaAmount() + " Tenticles");
-			popup.displayText("Thank you! I now have " + currentMonster.getTentaAmount() + " Tenticles");
-		}
+		currentMonster.setTentaAmout(currentMonster.getTentaAmount() + tentaGive);
+//		if(tentaGive == 0)
+//		{
+////			System.out.println("Ok, that's fine");
+//			popup.displayText("Ok, that's fine");
+//		}
+//		
+//		else if (tentaGive < 0)
+//		{
+////			System.out.println("Sorry but I won't let you take any of my tenticles.");
+//			popup.displayText("Sorry but I won't let you take any of my tenticles.");
+//		}
+//		else
+//		{
+//			currentMonster.setTentaAmout(currentMonster.getTentaAmount() + tentaGive);
+////			System.out.println("Thank you! I now have " + currentMonster.getTentaAmount() + " Tenticles");
+//			popup.displayText("Thank you! I now have " + currentMonster.getTentaAmount() + " Tenticles");
+//		}
 		
 		popup.displayText("Hi there, ready to play?");
 		String answer = popup.getResponse("What is the air speed of a coconut laden swallow");
-		System.out.println(response);
+		popup.displayText("Oh so it's " + answer + " Thanks");
 		
 
 		
